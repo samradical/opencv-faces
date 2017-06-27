0106c66f0b70b18166cea3784485be3d644f353e
samelie <sam@stinkdigital.com>
2017-06-27 10:20:58 -0700

    WIP

--- example.js
+++ example.js
@@ -1,3 +1,17 @@
-GetFacesFromClip('data/man2.avi').then(r => {
+const { GetFacesFromClip } = require('./index')
+GetFacesFromClip(
+  'data/fake2.mkv', {
+    doBackgrounds: false,
+    removeOriginalVideo: false,
+    dissimilartyFudge:.3, //decide how to group frames
+    minSecondsBetweenScenes: 1, //merge scenes if they are but x seconds apart
+    fps: 0.05, //half the fps,
+    outputDir: "output_frames",
+    outputImageHeight: "480",
+    imageExt: "png",
+    lookbackSecs: 1
+  }
+).then(r => {
   console.log(r);
+  console.log(r[0][0]);
 })
--- index.js
+++ index.js
@@ -1,16 +1,75 @@
 const Q = require('bluebird')
-const { ExtractFrames, FaceFromClip } = require('./lib/video-frames-faces')
+const { ExtractFrames, FaceFromClip,FacesFromVideo } = require('./lib/video-frames-faces')
 const GroupScenes = require('./lib/group-scenes')
 const CutScene = require('./lib/cut-scenes')
 const _ = require('lodash');
 
+
+const DEFAULT = {
+  dissimilartyFudge: .4,
+  minSecondsBetweenScenes: 1,
+}
+
 function groupAndCut(frames, options) {
   if (!frames) return null
-  return GroupScenes(frames)
+  return GroupScenes(frames, options)
     .then(scenes => {
+
+      let newScenes = []
+      let active = []
+
+      /*PUSH INTO ACTIVE ARR */
+
+      /*let i = 0
+      while (i < scenes.length) {
+        const scene = scenes[i]
+        const next = scenes[i + 1]
+        if (!next) {
+          active = []
+          active.push(scene)
+          newScenes.push(active)
+          i++
+        } else {
+          active = active || []
+          const { startTime } = next[0]
+          const { endTime } = scene[i]
+          console.log(startTime, endTime, options.minSecondsBetweenScenes);
+          if (endTime - startTime < options.minSecondsBetweenScenes) {
+            active
+            newScenes.push(_.flatten([...scene, ...next]))
+            i += 2
+          } else {
+            newScenes.push(scene)
+          }
+        }
+
+      }
+
+      for (var i = 0; i < scenes.length; i += 2) {
+        console.log("i", i);
+        const scene = scenes[i]
+        if (!next) {
+          newScenes.push(scene)
+        } else {
+          const { startTime } = next[0]
+          const { endTime } = _.last(scene)
+          console.log(startTime, endTime, options.minSecondsBetweenScenes);
+          if (endTime - startTime < options.minSecondsBetweenScenes) {
+            newScenes.push(_.flatten([...scene, ...next]))
+            i += 2
+          } else {
+            newScenes.push(scene)
+          }
+        }
+      }
+
+      console.log(newScenes);
+
+      console.log("---------");
+      console.log("---------");*/
+
       return CutScene(scenes, options)
         .then(videoScenes => {
-
           const s = scenes.map((faces, i) => ({
             videoSrc: videoScenes[i],
             faces: faces
@@ -20,19 +79,27 @@ function groupAndCut(frames, options) {
     })
 }
 
-const GetFacesFromClip = (videoPath, options = { doBackgrounds: true }) => {
+const AnalyzeVideo = (videoPath, options = {})=>{
 
-  return FaceFromClip(videoPath)
+}
+
+
+const GetFacesFromClip = (videoPath, options = { doBackgrounds: false }) => {
+
+  return FaceFromClip(videoPath, options)
     .then(allFrames => {
       const { doBackgrounds } = options
       return Q.all([
-        groupAndCut(allFrames.filter(frame => (frame.partOfFaceGroup)), { name: "faces" }),
-        (doBackgrounds ? groupAndCut(allFrames.filter(frame => (!frame.partOfFaceGroup)), { name: "background" }) : null),
-      ], { concurrency: 1 })
+          groupAndCut(allFrames.filter(frame => (frame.partOfFaceGroup)), _.assign({},DEFAULT, options, { name: "faces" })),
+          (doBackgrounds ? groupAndCut(allFrames.filter(frame => (!frame.partOfFaceGroup)), _.assign({},DEFAULT, options, { name: "background" })) : null),
+        ], { concurrency: 1 })
+        .then(results => (_.compact(_.flatten(results))))
 
     })
 
 }
 
 
-module.exports = GetFacesFromClip
+
+
+module.exports = {GetFacesFromClip, FacesFromVideo}
